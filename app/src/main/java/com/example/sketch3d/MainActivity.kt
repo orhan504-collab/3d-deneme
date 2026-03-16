342ed3fbe69925b8dfba514dae96378269025068
orhan504-collab <orhan_504@hotmail.com>
2026-03-17 01:19:11 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/sketch3d/MainActivity.kt
+++ app/src/main/java/com/example/sketch3d/MainActivity.kt
@@ -10,18 +10,13 @@ class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        // R.layout.activity_main, /res/layout/activity_main.xml dosyasını bağlar
         setContentView(R.layout.activity_main)
 
-        // XML'deki FrameLayout'u buluyoruz
         val container = findViewById<FrameLayout>(R.id.gl_container)
         glView = MyGLSurfaceView(this)
         container.addView(glView)
 
-        // Butonun tıklanmasını yönetiyoruz
-        val btnCube = findViewById<Button>(R.id.btn_cube)
-        btnCube.setOnClickListener {
+        findViewById<Button>(R.id.btn_cube).setOnClickListener {
             glView.renderer.toggleCube()
         }
     }
