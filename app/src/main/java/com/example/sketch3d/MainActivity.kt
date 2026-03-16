55380b57ff1f255d5f9e2e07b46ebeda456731fb
orhan504-collab <orhan_504@hotmail.com>
2026-03-17 01:41:36 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/sketch3d/MainActivity.kt
+++ app/src/main/java/com/example/sketch3d/MainActivity.kt
@@ -5,18 +5,13 @@ import android.os.Bundle
 import android.widget.FrameLayout
 
 class MainActivity : Activity() {
-    private lateinit var glView: MyGLSurfaceView
+    private var glView: MyGLSurfaceView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        // XML layout'u bağlıyoruz
         setContentView(R.layout.activity_main)
 
-        // XML'deki FrameLayout'u bul
         val container = findViewById<FrameLayout>(R.id.gl_container)
-        
-        // OpenGL görünümünü oluştur ve container'a ekle
         glView = MyGLSurfaceView(this)
         container.addView(glView)
     }
