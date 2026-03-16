66f96f69cfcfb7f5673b880c65884d61acba1a82
orhan504-collab <orhan_504@hotmail.com>
2026-03-17 00:36:55 +0300

    Update ShaderUtils.kt

--- app/src/main/java/com/example/sketch3d/ShaderUtils.kt
+++ app/src/main/java/com/example/sketch3d/ShaderUtils.kt
@@ -1,4 +1,5 @@
 package com.example.sketch3d
+
 import android.opengl.GLES20
 
 object ShaderUtils {
@@ -6,16 +7,14 @@ object ShaderUtils {
         val shader = GLES20.glCreateShader(type)
         GLES20.glShaderSource(shader, shaderCode)
         GLES20.glCompileShader(shader)
+        
+        // Hata kontrolü ekledik: Shader neden derlenemiyor?
+        val compiled = IntArray(1)
+        GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compiled, 0)
+        if (compiled[0] == 0) {
+            GLES20.glDeleteShader(shader)
+            return 0
+        }
         return shader
-    }
-
-    fun createProgram(vertexSource: String, fragmentSource: String): Int {
-        val vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexSource)
-        val fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentSource)
-        val program = GLES20.glCreateProgram()
-        GLES20.glAttachShader(program, vertexShader)
-        GLES20.glAttachShader(program, fragmentShader)
-        GLES20.glLinkProgram(program)
-        return program
     }
 }
