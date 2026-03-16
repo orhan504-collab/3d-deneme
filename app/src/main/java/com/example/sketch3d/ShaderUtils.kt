01c03e5affbea34b6cbb737dfc51ac773e6bee72
orhan504-collab <orhan_504@hotmail.com>
2026-03-16 20:03:21 +0300

    Create ShaderUtils.kt

---
+++ app/src/main/java/com/example/sketch3d/ShaderUtils.kt
@@ -0,0 +1,21 @@
+package com.example.sketch3d
+import android.opengl.GLES20
+
+object ShaderUtils {
+    fun loadShader(type: Int, shaderCode: String): Int {
+        val shader = GLES20.glCreateShader(type)
+        GLES20.glShaderSource(shader, shaderCode)
+        GLES20.glCompileShader(shader)
+        return shader
+    }
+
+    fun createProgram(vertexSource: String, fragmentSource: String): Int {
+        val vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexSource)
+        val fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentSource)
+        val program = GLES20.glCreateProgram()
+        GLES20.glAttachShader(program, vertexShader)
+        GLES20.glAttachShader(program, fragmentShader)
+        GLES20.glLinkProgram(program)
+        return program
+    }
+}
