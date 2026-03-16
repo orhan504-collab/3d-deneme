9117442dd22b17292b3a8acf6a217428fde52f69
orhan504-collab <orhan_504@hotmail.com>
2026-03-17 01:29:26 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/sketch3d/MainActivity.kt
+++ app/src/main/java/com/example/sketch3d/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.sketch3d
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.FrameLayout
 
 class MainActivity : Activity() {
@@ -10,14 +9,15 @@ class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        // XML layout'u bağlıyoruz
         setContentView(R.layout.activity_main)
 
+        // XML'deki FrameLayout'u bul
         val container = findViewById<FrameLayout>(R.id.gl_container)
+        
+        // OpenGL görünümünü oluştur ve container'a ekle
         glView = MyGLSurfaceView(this)
         container.addView(glView)
-
-        findViewById<Button>(R.id.btn_cube).setOnClickListener {
-            glView.renderer.toggleCube()
-        }
     }
 }
