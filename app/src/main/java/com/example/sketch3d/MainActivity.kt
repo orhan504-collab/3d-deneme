36dd0aef94b48663e9809ec34f6adcbf2cac485e
orhan504-collab <orhan_504@hotmail.com>
2026-03-17 01:20:19 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/sketch3d/MainActivity.kt
+++ app/src/main/java/com/example/sketch3d/MainActivity.kt
@@ -1,29 +1,23 @@
 package com.example.sketch3d
 
-import android.opengl.GLES20
-import android.opengl.GLSurfaceView
-import javax.microedition.khronos.egl.EGLConfig
-import javax.microedition.khronos.opengles.GL10
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.FrameLayout
 
-class MyRenderer : GLSurfaceView.Renderer {
-    private var showCube = false
+class MainActivity : Activity() {
+    private lateinit var glView: MyGLSurfaceView
 
-    fun toggleCube() { 
-        showCube = !showCube 
-    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
-        GLES20.glClearColor(0.2f, 0.2f, 0.2f, 1.0f)
-    }
+        val container = findViewById<FrameLayout>(R.id.gl_container)
+        glView = MyGLSurfaceView(this)
+        container.addView(glView)
 
-    override fun onDrawFrame(gl: GL10?) {
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT or GLES20.GL_DEPTH_BUFFER_BIT)
-        if (showCube) {
-            // İleride küp çizim kodlarını buraya ekleyeceğiz
+        findViewById<Button>(R.id.btn_cube).setOnClickListener {
+            glView.renderer.toggleCube()
         }
-    }
-
-    override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
-        GLES20.glViewport(0, 0, width, height)
     }
 }
