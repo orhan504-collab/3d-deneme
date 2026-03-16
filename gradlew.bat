05df9918599d773d4464c2572981bf4da1bce3a5
orhan504-collab <orhan_504@hotmail.com>
2026-03-16 20:21:26 +0300

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,7 +1,2 @@
 @echo off
-setlocal
-set DIRNAME=%~dp0
-if "%DIRNAME%" == "" set DIRNAME=.
-set APP_HOME=%DIRNAME%
-set APP_OPTS=-Xmx64m -Xms64m
-java %APP_OPTS% -classpath "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -jar gradle\wrapper\gradle-wrapper.jar %*
