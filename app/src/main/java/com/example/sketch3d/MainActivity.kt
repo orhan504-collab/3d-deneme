7fdb241b2f5086f9aad15a5816f8ab2cb083b50d
orhan504-collab <orhan_504@hotmail.com>
2026-03-17 00:59:38 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/sketch3d/MainActivity.kt
+++ app/src/main/java/com/example/sketch3d/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.example.sketch3d
 
 import android.app.Activity
-import android.opengl.GLSurfaceView
 import android.os.Bundle
+import android.widget.Button
+import android.widget.FrameLayout
 
 class MainActivity : Activity() {
-    private lateinit var gLView: GLSurfaceView
+    private lateinit var glView: MyGLSurfaceView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        gLView = GLSurfaceView(this)
-        gLView.setEGLContextClientVersion(2)
-        gLView.setRenderer(MyRenderer()) // Bu artık dışarıdaki MyRenderer.kt'yi kullanacak
-        setContentView(gLView)
+        setContentView(R.layout.activity_main)
+
+        // XML'deki FrameLayout'u bul
+        val container = findViewById<FrameLayout>(R.id.gl_container)
+        glView = MyGLSurfaceView(this)
+        container.addView(glView)
+
+        // Butonları tanımla
+        findViewById<Button>(R.id.btn_cube).setOnClickListener {
+            glView.renderer.toggleCube()
+        }
     }
 }
