c7d30e5b74bede670df327dd4b5aacb3120aac1c
orhan504-collab <orhan_504@hotmail.com>
2026-03-17 01:38:06 +0300

    Update MyGLSurfaceView.kt

--- app/src/main/java/com/example/sketch3d/MyGLSurfaceView.kt
+++ app/src/main/java/com/example/sketch3d/MyGLSurfaceView.kt
@@ -2,13 +2,35 @@ package com.example.sketch3d
 
 import android.content.Context
 import android.opengl.GLSurfaceView
+import android.view.MotionEvent
 
 class MyGLSurfaceView(context: Context) : GLSurfaceView(context) {
     val renderer = MyRenderer()
+    private var previousX: Float = 0f
+    private var previousY: Float = 0f
 
     init {
         setEGLContextClientVersion(2)
         setRenderer(renderer)
         renderMode = RENDERMODE_CONTINUOUSLY
+    }
+
+    override fun onTouchEvent(e: MotionEvent): Boolean {
+        val x: Float = e.x
+        val y: Float = e.y
+
+        when (e.action) {
+            MotionEvent.ACTION_MOVE -> {
+                val dx: Float = x - previousX
+                val dy: Float = y - previousY
+                
+                renderer.angleX += dx * 0.3f
+                renderer.angleY += dy * 0.3f
+                requestRender()
+            }
+        }
+        previousX = x
+        previousY = y
+        return true
     }
 }
