2a3184eff0d157d2ba81572cf6f2390173300505
orhan504-collab <orhan_504@hotmail.com>
2026-03-17 01:19:31 +0300

    Update MyGLSurfaceView.kt

--- app/src/main/java/com/example/sketch3d/MyGLSurfaceView.kt
+++ app/src/main/java/com/example/sketch3d/MyGLSurfaceView.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.opengl.GLSurfaceView
 
 class MyGLSurfaceView(context: Context) : GLSurfaceView(context) {
-    // 'private' anahtar kelimesini kaldırdık ki dışarıdan erişilebilsin
     val renderer = MyRenderer()
 
     init {
